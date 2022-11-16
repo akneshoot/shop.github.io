0fd3c3ac8ef22429df0cb850354a5d19e529c43f
akneshoot <98964746+akneshoot@users.noreply.github.com>
2022-11-17 01:01:32 +0300

    Add files via upload

---
+++ loginscript.js
@@ -0,0 +1,17 @@
+let result;
+
+
+while(true)
+{
+    result = prompt("Желаете пройти регистрацию на сайте?");
+
+    if (result == "Да")
+    {
+        alert("Круто!");
+        break;
+    }
+    else
+    {
+        alert("Попробуйте еще раз");
+    }
+};
